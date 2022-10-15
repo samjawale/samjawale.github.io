cfaf7661ed9ddfeb6c5c599204a306f994c8ce22
Sam <jawalesumit@gmail.com>
2022-10-15 00:27:15 -0700

    add error page arts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 import { DefaultTheme, ThemeProvider } from "styled-components";
 import GlobalStyle from "@/components/styles/GlobalStyle";
 import AppLayout from "@/components/AppLayout";
+import Page500 from "@/components/pages/error/Page500";
 import { darkTheme, lightTheme } from "@/helpers/styles";
 import useThemeDetector from "@/hooks/useThemeDetector";
 import { ThemeKind } from "@/types/style";
@@ -12,15 +14,17 @@ const App = () => {
   const [currentTheme, setCurrentTheme] = useState<DefaultTheme>(isSystemThemeDark ? darkTheme : lightTheme);
 
   return (
-    <ThemeProvider theme={currentTheme}>
-      <BrowserRouter>
-        <GlobalStyle {...currentTheme} />
-        <AppLayout
-          isDarkTheme={currentTheme.kind === ThemeKind.DARK}
-          toggleTheme={() => setCurrentTheme(currentTheme.kind === ThemeKind.DARK ? lightTheme : darkTheme)}
-        />
-      </BrowserRouter>
-    </ThemeProvider>
+    <ErrorBoundary FallbackComponent={Page500}>
+      <ThemeProvider theme={currentTheme}>
+        <BrowserRouter>
+          <GlobalStyle {...currentTheme} />
+          <AppLayout
+            isDarkTheme={currentTheme.kind === ThemeKind.DARK}
+            toggleTheme={() => setCurrentTheme(currentTheme.kind === ThemeKind.DARK ? lightTheme : darkTheme)}
+          />
+        </BrowserRouter>
+      </ThemeProvider>
+    </ErrorBoundary>
   );
 };
 
--- src/components/pages/error/Page500.tsx
+++ src/components/pages/error/Page500.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { FallbackProps } from "react-error-boundary";
 import styled from "styled-components";
 import { Button, Result, Typography } from "antd";
 import { HomeOutlined } from "@ant-design/icons";
@@ -9,15 +10,16 @@ const { Title } = Typography;
 const StyledTitle = styled(Title)`
   color: ${props => props.theme.color.font.primary} !important;
 `;
-const title = <StyledTitle>404</StyledTitle>;
-const subTitle = <StyledTitle level={4}>Uh-oh! The page you're looking for does not exist.</StyledTitle>;
+const title = <StyledTitle>500</StyledTitle>;
+const subTitle = <StyledTitle level={4}>Uh-oh! Sorry, something went wrong.</StyledTitle>;
 
-const Page404 = () => {
+type Props = FallbackProps;
+const Page500 = ({ error, resetErrorBoundary }: Props) => {
   const navigate = useNavigate();
 
   return (
     <Result
-      status="404"
+      status="500"
       title={title}
       subTitle={subTitle}
       extra={
@@ -29,4 +31,4 @@ const Page404 = () => {
   );
 };
 
-export default Page404;
+export default Page500;
